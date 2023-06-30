806434b5db1be0a6dea03b2495ac7eac9f1494c5
m1chaelwilliams <michaelwilliamspersonal@gmail.com>
2023-06-30 10:33:03 -0500

    first commit

---
+++ src/routes/docs/+page.svelte
@@ -0,0 +1,18 @@
+<div class="page bg-dark">
+    <section class="bg-dark">
+        <h1>Documentation</h1>
+        <h2>Keep it simple</h2>
+    </section>
+    
+    <section class="bg-primary content">
+        <p>No crazy overhead here. Most of these components are semantic and easy to use. Just copy and paste them into your new/existing project and have fun!</p>
+    </section>
+</div>
+
+<style>
+    .content {
+        width: 80%;
+        margin: auto;
+        border-radius: 1em;
+    }
+</style>
